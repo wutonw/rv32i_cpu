39930f7e5a01fe35ef11c1e1fde48c0e60dd33c6
陈 啥 <wutongszmc@163.com>
2026-09-02 10:22:14 +0800

    add if/id reg

--- src/core/pc_reg.v
+++ src/core/pc_reg.v
@@ -1,39 +1,16 @@
 module pc_reg(
     input wire clk,
     input wire rst_n,
-    
+
     input wire stall,
-    input wire branch,
-    input wire jump,
-    input wire jump_reg,
-
-    input wire [31:0] imm,
-    input wire [31:0] alu_result,
-
-    input wire trap_enter,
-    input wire trap_exit,
-    input wire [31:0] trap_vector,
-    input wire [31:0] mepc_out,
-
+    input wire [31:0] next_pc,
     output reg [31:0] pc
 );
     always @(posedge clk or negedge rst_n)begin
         if(!rst_n)begin
-            pc <= 32'b0;
-        end else if(trap_enter)begin
-                pc <= trap_vector;
-        end else if (trap_exit)begin
-                pc <= mepc_out;
-        end else begin
-            if (stall)begin
-                pc <= pc;
-            end else if( (branch && alu_result[0]) || jump)begin
-                pc <= pc + imm;
-            end else if (jump_reg)begin
-                pc <= {alu_result[31:1],1'b0};
-            end else begin
-                pc <= pc + 32'd4;
-            end
+            pc <= 32'h0000_0000;
+        end else if(!stall)begin
+            pc <= next_pc;
         end
     end
 endmodule
---
+++ src/core/pipe_if_id.v
@@ -0,0 +1,23 @@
+module pipe_if_id(
+    input wire clk,
+    input wire rst_n,
+    input wire stall,
+
+    input wire [31:0] pc,
+    input wire [31:0] inst,
+    output wire [31:0] if_id_inst,
+    output reg [31:0] if_id_pc,
+    output reg if_id_valid
+);
+
+    always @(posedge clk or negedge rst_n)begin
+        if(!rst_n)begin
+            if_id_pc <= 32'h0000_0000;
+            if_id_valid <= 1'b0;
+        end else if(!stall)begin
+            if_id_pc <= pc;
+            if_id_valid <= 1'b1;
+        end
+    end
+    assign if_id_inst = inst;
+endmodule
